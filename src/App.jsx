bd9d15347ac7dc05a35a7fc621ea094dc6972efe
Eamon McHugh <eamonmchugh2@gmail.com>
2022-08-24 12:21:41 -0400

    meta, SCO and favicon edits

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,29 @@ import About from './components/About/About';
 import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
 
+// SCO
+import { Helmet } from 'react-helmet';
+
 function App() {
 	return (
 		<>
+			<Helmet>
+				<meta charSet='utf-8' />
+				<title>Eamon McHugh's Portfolio</title>
+				<link
+					rel='canonical'
+					href='https://eamon02.github.io/react-portfolio-purple/'
+				/>
+				<meta
+					name='description'
+					content="Eamon McHugh's Portfolio"
+				/>
+			</Helmet>
+
 			<Header />
 			<About />
 			<Projects />
-			<Contact/>
+			<Contact />
 		</>
 	);
 }
